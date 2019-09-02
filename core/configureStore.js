766f52bf6f6488e956bc2bb5301b83dfe513e09b
unknown <luexe@yandex.ru>
2019-09-02 18:13:43 +0300

    f

---
+++ core/configureStore.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import rootSaga from "./sagas";
+import rootReducer from "./reducers";
+
+const sagaMiddleware = createSagaMiddleware();
+
+export function configureStore(initialState) {
+  const middleware = [sagaMiddleware];
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middleware))
+  );
+  sagaMiddleware.run(rootSaga);
+  return store;
+}
